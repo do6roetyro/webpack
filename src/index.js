f981c2d6a0a239645ad87bc8877d7bd116288c03
do6roetyro <do6roeytro@yandex.ru>
2023-12-04 20:43:03 +0300

    v.1.0.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/app";
+
+import "./styles/main.scss";
+import "./styles/fonts.scss";
+
 function component(text) {
-    const element = document.createElement('h1');
-    element.textContent = text;
-    return element;
-  }
-  
-  document.body.prepend(component('Проект собран на Webpack'));
+  const element = document.createElement("h1");
+  element.textContent = text;
+  return element;
+}
+
+document.body.prepend(component("Проект собран на Webpack"));
+
+// ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "development", // или 'production'
@@ -8,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.[contenthash].js",
+    assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
   },
   module: {
     rules: [
@@ -16,27 +19,42 @@ module.exports = {
         use: "babel-loader",
         exclude: /node_modules/,
       },
-      // {
-      //     test: /\.scss$/,
-      //     use: ['style-loader', 'css-loader', 'sass-loader'],
-      //   },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
+      {
+        test: /\.(scss|css)$/,
         use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "images",
-            },
-          },
-          "image-webpack-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
+      },
+      {
+        test: /\.(woff2?|woff|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[contenthash][ext]", // изменение здесь
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+        generator: {
+          filename: path.join("icons", "[name].[contenthash][ext]"),
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"), // ваш шаблон HTML
+      template: path.join(__dirname, "src", "index.pug"), // ваш шаблон HTML
       filename: "index.html",
     }),
     new FileManagerPlugin({
@@ -44,11 +62,39 @@ module.exports = {
         onStart: {
           delete: ["dist"],
         },
+        onEnd: {
+          copy: [
+            {
+              source: path.join("src", "static"),
+              destination: "dist",
+            },
+          ],
+        },
       },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
     }),
   ],
   devServer: {
     watchFiles: path.join(__dirname, "src"),
     port: 9000,
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { name: "preset-default" }],
+            ],
+          },
+        },
+      }),
+    ],
   },
 };
